d64cb487f1dfbd3ec6ee21a535f9c6aecb9b7a64
Yuichi424 <113126646+Yuichi424@users.noreply.github.com>
2022-09-22 09:46:51 -0400

    initial commit

---
+++ routes/index.js
@@ -0,0 +1,25 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });//render is to rendering to show user the readable form
+});
+  router.get('/', function(req, res, next) {
+    res.render('index', { title: 'Home' });
+});
+  router.get('about', function(req, res, next) {
+    res.render('index', { title: 'About' });
+});
+  router.get('Products', function(req, res, next) {
+    res.render('index', { title: 'Products' });
+});
+  router.get('Services', function(req, res, next) {
+    res.render('index', { title: 'Servicies' });
+});
+  router.get('Contacts', function(req, res, next) {
+    res.render('index', { title: 'Contacts' });
+  
+});
+
+module.exports = router;

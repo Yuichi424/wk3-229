0a3244876985e2b75a3c0ebe33a985edeab1dc29
Yuichi424 <113126646+Yuichi424@users.noreply.github.com>
2022-09-22 11:40:57 -0400

    update index.ejs

--- routes/index.js
+++ routes/index.js
@@ -5,19 +5,19 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });//render is to rendering to show user the readable form
 });
-  router.get('/', function(req, res, next) {
+  router.get('/home', function(req, res, next) {
     res.render('index', { title: 'Home' });
 });
-  router.get('about', function(req, res, next) {
+  router.get('/about', function(req, res, next) {
     res.render('index', { title: 'About' });
 });
-  router.get('Products', function(req, res, next) {
+  router.get('/Products', function(req, res, next) {
     res.render('index', { title: 'Products' });
 });
-  router.get('Services', function(req, res, next) {
+  router.get('/Services', function(req, res, next) {
     res.render('index', { title: 'Servicies' });
 });
-  router.get('Contacts', function(req, res, next) {
+  router.get('/Contacts', function(req, res, next) {
     res.render('index', { title: 'Contacts' });
   
 });
